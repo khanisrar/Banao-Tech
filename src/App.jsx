925f6508bdb4a2400a02a4a31aa239407590c50e
khanisrar <khan.israr.lap.7651@gmail.com>
2024-02-17 21:56:57 +0530

    banao tech assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './Components/Navbar';
 import Home from './Components/Home';
-import UserDetails from './Components/UserDetails';
 
 
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/user-details/:id' element={<UserDetails />} />
-        </Routes>
-      </BrowserRouter>
+      <Navbar />
+      <Home />
     </>
   );
 };
